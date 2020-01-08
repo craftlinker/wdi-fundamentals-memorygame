7c12a9040c1cdd1fa647ebda3793edd47e4fc72b
craftelinker <brandonDustin@Brandons-MacBook-Pro.local>
2020-01-08 15:20:43 -0600

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,29 @@
-console.log("Up and running!");
 
-let cardOne = 'queen';
-let cardTwo = 'king';
-let cardThree = 'king';
-let cardFour = 'queen';
 
-console.log('User fliped' +' '+ cardOne);
+
+
+
+alert('you found a match!')
+
+let cards=['queen', 'queen', 'king', 'king'];
+
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log('User fliped queen');
+
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log('User fliped king');
+
+if(cardsInPlay.length === 2) {
+    console.log(true)
+}
+else if( cardsInPlay[0] === cardsInPlay[1]){
+    alert(' You found a match!')
+}
