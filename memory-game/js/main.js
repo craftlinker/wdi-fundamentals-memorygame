5293f2a5339252595e64f5e11ddb97e1f35ef59c
craftelinker <brandonDustin@Brandons-MBP.home>
2020-01-09 22:12:55 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,25 +2,47 @@
 
 
 
-let cards=['queen', 'queen', 'king', 'king'];
+let cards=[
+    {
+        rank:"queen",
+        suit:"hearts",
+        cardImage:"images/queen-of-hearts.png"
+    },
+    {
+        rank:"queen",
+        suit:"diamonds"	,
+        cardImage:"images/queen-of-diamonds.png"
+    },
+    {
+        rank:"kings",
+        suit:"hearts",
+        cardImage:"images/king-of-hearts.png"
+    },
+    {
+        rank:"kings",
+        suit:"diamonds",
+        cardImage:"images/king-of-diamonds.png"
+    }
+];
+
 let cardsInPlay = [];
 function checkForMatch(){
     
     if (cardsInPlay[0] === cardsInPlay[1]) {
         console.log("You found a match!");
     } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
     }    
     
-
 };
 
 
 function flipCard(cardId){
    
     
-console.log(" User Flipped " + " " + cards[cardId]);
-cardsInPlay.push(cards[cardId])
+console.log(" User Flipped " + " " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank)
+console.log(cards[cardId].cardImage) + " " + console.log(cards[cardId].suit)
 checkForMatch();
 
 };
